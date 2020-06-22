074ab1c315078d68987a7ea9d0a5db2c1958799b
unknown <bruno_cunha@usp.br>
2020-06-22 18:39:07 -0300

    Up

--- main.js
+++ main.js
@@ -12,3 +12,15 @@ let contarCaracteres = () => {
     let texto = document.getElementById("texto")
     document.getElementById("quantidade").innerHTML = texto.value.length + ' caractere(s)';
 }
+
+let celsiusConv = (c) => {
+    document.getElementById("f").innerHTML = 'ºF = ' + ((parseFloat(c) * 9.0/5.0) + 32)
+    document.getElementById("k").innerHTML = 'K = ' + (parseFloat(c) + 273.15)
+}
+
+let polegadasConv = (p) => {
+    document.getElementById("mm").innerHTML = 'mm = ' + (parseFloat(p) * 25.4)
+    document.getElementById("milha").innerHTML = 'Milhas = ' + (parseFloat(p) / 63360)
+    document.getElementById("jarda").innerHTML = 'Jarda = ' + (parseFloat(p) / 36)
+    document.getElementById("pe").innerHTML = 'Pé = ' + (parseFloat(p) / 12)
+}
